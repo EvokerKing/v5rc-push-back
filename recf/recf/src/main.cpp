960caac4fcdee5558b14e773dd49c13f17b24761
miele <maxwellgsfarm@gmail.com>
2026-01-22 15:22:37 -0500

    updates

--- recf/recf/src/main.cpp
+++ recf/recf/src/main.cpp
@@ -14,7 +14,7 @@ lemlib::Drivetrain drivetrain(
 	320,
 	4
 );
-pros::Imu imu(7);
+pros::Imu imu(13);
 lemlib::OdomSensors sensors(
 	nullptr,
 	nullptr,
@@ -23,10 +23,10 @@ lemlib::OdomSensors sensors(
 	&imu
 );
 lemlib::ControllerSettings lateral_controller(
-	0,
-	0,
-	0,
-	0,
+	75,
+	40,
+	2,
+	0.2,
 	0,
 	0,
 	0,
@@ -34,9 +34,9 @@ lemlib::ControllerSettings lateral_controller(
 	0
 );
 lemlib::ControllerSettings angular_controller(
+	2,
 	0,
-	0,
-	0,
+	15,
 	0,
 	0,
 	0,
@@ -50,29 +50,35 @@ lemlib::Chassis chassis(
 	angular_controller,
 	sensors
 );
-lemlib::PID levelPID(0, 0, 0);
+lemlib::PID levelPID(4, 0, 16);
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
-pros::Motor level(8, pros::v5::MotorGears::red);
-pros::Rotation rotation(9);
-pros::Motor intake(-10);
+pros::Motor level(14, pros::v5::MotorGears::red);
+pros::Rotation rotation(12);
+pros::Motor intake(-7);
 pros::adi::AnalogOut alignment(1);
+pros::adi::DigitalIn lever(8);
 
-int target[] = { 0, 3000, 4500 };
+int start_position;
+int target[] = { 0, 5000, 8000 };
 std::vector<char*> instr;
 
 void initialize() {
 	pros::lcd::initialize();
 	chassis.calibrate();
-	rotation.reset_position();
-	FILE* file = fopen("/usd/auton.rbs", "r");
-	char buf[10000];
-	fread(buf, 1, 10000, file);
-	std::printf("%s\n", buf);
-	char* add = strtok(buf, "\n");
-	do {
-		instr.push_back(add);
-		add = strtok(NULL, "\n");
-	} while (add);
+	rotation.reset();
+	start_position = rotation.get_position();
+
+	FILE* file = fopen(lever.get_value() ? "/usd/autonL.rbs" : "/usd/autonR.rbs", "r");
+	if (file != nullptr) {
+		char buf[10000];
+		fread(buf, 1, 10000, file);
+		std::printf("%s\n", buf);
+		char* add = strtok(buf, "\n");
+		do {
+			instr.push_back(add);
+			add = strtok(NULL, "\n");
+		} while (add);
+	}
 }
 
 void autonomous() {
@@ -108,7 +114,7 @@ void autonomous() {
 		} else if (tok[0] == "LEVEL") {
 			bool close;
 			do {
-				int current = rotation.get_position();
+				int current = rotation.get_position() - start_position;
 				int error;
 				if (tok[1] == "up") {
 					error = target[2] - current;
@@ -119,7 +125,7 @@ void autonomous() {
 				} else {
 					throw 1;
 				}
-				level.move_relative(levelPID.update(error), 100);
+				level.move(levelPID.update(error));
 				close = error <= 100;
 			} while (!close);
 		} else if (tok[0] == "ALIGN") {
@@ -132,6 +138,7 @@ void opcontrol() {
 	bool intake_moving = false;
 	bool last_changed = false;
 	bool aligned = false;
+	int moveTo = -1;
 
 	intake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 	level.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
@@ -140,6 +147,7 @@ void opcontrol() {
 		pros::lcd::print(0, "X: %f", chassis.getPose().x);
 		pros::lcd::print(1, "Y: %f", chassis.getPose().y);
 		pros::lcd::print(2, "Theta: %f", chassis.getPose().theta);
+		pros::lcd::print(3, "Level: %d", rotation.get_position() - start_position);
 
 		int left_thumbstick = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 		int right_thumbstick = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
@@ -161,17 +169,35 @@ void opcontrol() {
 			intake.brake();
 		}
 
-		int current = rotation.get_position();
+		int current = rotation.get_position() - start_position;
 		if (controller.get_digital(DIGITAL_L2) == 1) {
 			level.move(-127);
 		} else if (controller.get_digital(DIGITAL_R2) == 1) {
 			level.move(127);
-		} else if (controller.get_digital(DIGITAL_UP) == 1) {
-			level.move_relative(levelPID.update(target[2] - current), 100);
-		} else if (controller.get_digital(DIGITAL_LEFT) == 1) {
-			level.move_relative(levelPID.update(target[1] - current), 100);
-		} else if (controller.get_digital(DIGITAL_DOWN) == 1) {
-			level.move_relative(levelPID.update(target[0] - current), 100);
+		} else if (controller.get_digital(DIGITAL_UP) == 1 || moveTo == 2) {
+			level.move(levelPID.update(current - target[2]));
+			if (abs(target[2] - current) <= 100) {
+				moveTo = -1;
+				level.brake();
+			} else {
+				moveTo = 2;
+			}
+		} else if (controller.get_digital(DIGITAL_LEFT) == 1 || moveTo == 1) {
+			level.move(levelPID.update(current - target[1]));
+			if (abs(target[1] - current) <= 100) {
+				moveTo = -1;
+				level.brake();
+			} else {
+				moveTo = 1;
+			}
+		} else if (controller.get_digital(DIGITAL_DOWN) == 1 || moveTo == 0) {
+			level.move(levelPID.update(current - target[0]));
+			if (abs(target[0] - current) <= 100) {
+				moveTo = -1;
+				level.brake();
+			} else {
+				moveTo = 0;
+			}
 		} else {
 			level.move(0);
 			level.brake();
